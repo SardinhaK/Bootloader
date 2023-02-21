b305f986c52a6ec6d2871f906c735bdab42eb82c
Thyago Santos <97205737+ThyagoSnt@users.noreply.github.com>
2023-02-21 14:50:18 -0300

    Add files via upload

--- speedRun/kernel.asm
+++ speedRun/kernel.asm
@@ -1,8 +1,12 @@
 org 0x7e00
 jmp 0x0000:Titulo
+
 ; VARIAVEIS USADAS NO KERNEL
 data:
-  ;TITULO
+
+  random_seed dd 0 ;SEED ALEATORIA
+
+;TITULO
     nomeDoGame dw '             BETRACHTUNG', 0
     press dw      '     pressione enter para continuar!', 0
 
@@ -23,13 +27,13 @@ data:
   
   ;CREDITOS
     credito1 dw 'Desenvolvido para a cadeira de:  ', 0
-    credito2 dw 'INFRAESTRUTURA DE SOFTWARE', 0
+    credito2 dw '      INFRAESTRUTURA DE SOFTWARE', 0
     credito3 dw 'Discentes envolvidos: ', 0
     credito4 dw 'Emanoel Thyago Cordeiro dos Santos-', 0
     credito5 dw 'etcs', 0
     credito6 dw 'Pericles Ferreira Sardinha - ', 0
     credito7 dw 'pfs', 0
-    credito8 dw 'Rafael Ramos B Cordeiro - ', 0
+    credito8 dw 'Rafael Ramos B Corcino - ', 0
     credito9 dw 'rrbc', 0
     creditoR dw '            Enter para retornar ao Menu', 0
 
@@ -38,8 +42,26 @@ data:
     jogo1 dw '                    ESPERE', 0
     jogo2 dw '                     AGORA', 0 
     jogo3 dw 'Sua pontuacao foi: ', 0
+
+    MAX_TIME equ 5000 ; Define a constante MAX_TIME com o valor 5000 (em milissegundos)
        
-;FUNÇÕES USADAS 
+;FUNÇÕES USADAS
+
+; gera um número aleatório entre min_val e max_val
+generate_random_number:
+  mov eax, [random_seed]    ; carrega a seed em eax
+  imul eax, 1103515247      ; multiplica a seed por uma constante grande
+  add eax, 12349           ; adiciona uma constante
+  mov [random_seed], eax    ; salva a nova seed
+
+  mov ebx, 7                ; define o limite
+  xor edx, edx              ; limpa edx (resto)
+  div ebx                   ; divide eax por 7, o resto vai pra edx
+  inc edx                   ; adiciona 1 no resto para obter o random
+
+  mov eax, edx              ;salva o random em eax
+  ret
+
 printImagem:
   xor ax, ax
   mov ds, ax
@@ -72,6 +94,7 @@ printImagem:
 		jmp .for1
 	.endfor1:
   ret
+  
 putchar:    ;Printa um caractere na tela, pega o valor salvo em al
   mov ah, 0x0e
   int 10h
@@ -102,6 +125,17 @@ delay100ms:              ; 0.1 SEC DELAY
   mov ah, 86h
   int 15h
   ret
+
+Espacamento:
+  loop_Espaco:
+    call endl       ; Pula uma linha
+    dec bh         ; Decrementa bh em um
+    cmp bh, 0    ; Compara bh com zero
+    je exit_loop    ; Se zerar vai pro final
+    jmp loop_Espaco ; Caso contrário loop novamente
+
+  exit_loop:
+    ret         ; Retorna pro ponto de chamada
 
 prints:             ; mov si, string
   .loop:
@@ -142,6 +176,7 @@ esperaOenter:
 
   ; Limpa o buffer do teclado
   ret
+  
 print_word:
     push bx             ; salva a base na pilha
     xor cx, cx          ; zera o contador
@@ -161,8 +196,11 @@ print_word:
         loop print_loop ; repete o loop enquanto ainda houver caracteres na pilha
     pop bx              ; restaura a base da pilha
     ret
+
+
 ;TELA DE TITULO
 Titulo:
+
   ;DEFININDO O MODO DE VIDEO
   mov ah, 0      ; ALTERANDO A RESOLUÇÃO E TAMANHO DAS LETRAS
   mov al, 13h
@@ -172,24 +210,13 @@ Titulo:
   mov bl, 4 ; ESCOLHE A COR DA LETRA A SER PRINTADA
   int 10h  ; CHAMA A INTERRUPÇÃO PARA TROCAR A COR
   
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
+  mov bh, 8 ;numero de espaços
+  call Espacamento ;pulanndo 8 espaços
 
   mov si, nomeDoGame
   call prints
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
+  mov bh, 7 ;numero de espaços
+  call Espacamento ;pulanndo 7 espaços
 
   mov si, press
   call prints
@@ -208,27 +235,18 @@ menu:
   int 10h
   mov si, nomeDoGame
   call prints
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
+  mov bh, 7 ;numero de espaços
+  call Espacamento ; pulando 7 espaços
   mov bl, 1
   int 10h
   mov si, jogar
   call prints
-  call endl
-  call endl
-  call endl
-  call endl
+  mov bh, 4 ;numero de espaços
+  call Espacamento ;pulando 4 espaços
   mov si, comandos
   call prints
-  call endl
-  call endl
-  call endl
-  call endl
+  mov bh, 4 ;numero de espaços
+  call Espacamento ;pulando 4 espaços
   mov si, creditos
   call prints
   leituraMenu:
@@ -254,73 +272,62 @@ play:
     mov bl, 4 ; COR DO FUNDO DE TELA (VERMELHO)
     int 10h ; 
 
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
+    mov bh, 12 ;numero de espaços
+    call Espacamento ;pulanndo 12 espaços
     mov bl, 15
     int 10h
     mov si, jogo1
     call prints
-
-    call delay1s
-
-    call clear
-    mov ah,0xb
-    mov bh, 0
-    mov bl, 2 ; COR DO FUNDO DE TELA (VERDE)
-    int 10h ; 
-
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    call endl
-    mov bl, 15
-    int 10h
-    mov si, jogo2
-    call prints
-    mov dx, contador
-  esperandoClique:
-    call delay100ms
-    call getchar
-    cmp al, 0x20
-    je pontoTotal
-    sub dx, 1
-    jmp esperandoClique
-
-  pontoTotal:
-    call clear
-    mov ah,0xb
-    mov bh, 0
-    mov bl, 1 ; COR DO FUNDO DE TELA (VERDE)
-    int 10h ; 
     
-    call endl
-    call endl
-    mov bl, 15 ; COR da letra
-    int 10h ; 
-    mov si, jogo3
-    call prints
-    mov si, dx
-    call prints
-   
-    jmp done
+    call generate_random_number ;random em ax
+    mov bl, al
+
+    loop_delay:
+      call delay1s
+      dec bl
+      cmp bl, 0
+      jle continue
+      jmp loop_delay
+    
+    continue:  
+      call clear
+      mov ah,0xb
+      mov bh, 0
+      mov bl, 2 ; COR DO FUNDO DE TELA (VERDE)
+      int 10h ; 
+
+      mov bh, 12 ;numero de espaços
+      call Espacamento ;pulanndo 12 espaços
+      mov bl, 15
+      int 10h
+      mov si, jogo2
+      call prints
+      mov dx, contador
+    esperandoClique:
+      call getchar
+      cmp al, 0x20
+      je pontoTotal
+      sub dx, 1
+      jmp esperandoClique
+
+
+    pontoTotal:
+      call clear
+      mov ah,0xb
+      mov bh, 0
+      mov bl, 1 ; COR DO FUNDO DE TELA (VERDE)
+      int 10h ; 
+      
+      call endl
+      call endl
+      mov bl, 15 ; COR da letra
+      int 10h ; 
+      mov si, jogo3
+      call prints
+      mov si, dx
+      call prints
+      call esperaOenter
+      jmp Titulo
 
 ;TELA DE COMANDOS
 comando:
@@ -358,20 +365,8 @@ comando:
   call endl
   mov si, comoJogar8
   call prints
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
+  mov bh, 14 ;numero de espaços
+  call Espacamento ;pulanndo 14 espaços
   mov bl, 15
   int 10h
   mov si, creditoR
@@ -392,21 +387,17 @@ credito:
   int 10h
   mov si, credito2
   call endl
+  call endl
   call prints
-  call endl
-  call endl
-  call endl
-  call endl
+  mov bh, 4 ;numero de espaços
+  call Espacamento ;pulanndo 4 espaços
   int 10h
   mov si, credito3
   mov bl, 15
   int 10h
   call prints
-  call endl 
-  call endl
-  call endl
-  call endl
-  call endl
+  mov bh, 4 ;numero de espaços
+  call Espacamento ;pulanndo 4 espaços
   mov si, credito4
   mov bl, 1
   call prints
@@ -424,9 +415,8 @@ credito:
   int 10h
   mov si, credito7
   call prints
-  call endl
-  call endl
-  call endl
+  mov bh, 3 ;numero de espaços
+  call Espacamento ;pulanndo 3 espaços
   mov bl, 1
   int 10h
   mov si, credito8
@@ -435,11 +425,8 @@ credito:
   int 10h
   mov si, credito9
   call prints
-  call endl
-  call endl
-  call endl
-  call endl
-  call endl
+  mov bh, 5 ;numero de espaços
+  call Espacamento ;pulanndo 5 espaços
   mov bl, 15
   int 10h
   mov si, creditoR
